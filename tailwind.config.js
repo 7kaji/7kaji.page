21b5ddca504405b2bd8205ebfdc4547b2b7f4331
7kaji <hirotonbuu@gmail.com>
2020-10-12 01:24:52 +0900

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  future: 'all',
+  experimental: 'all',
+  purge: ['./pages/**/*.js', './components/**/*.js'],
+  variants: {},
+  plugins: [require('@tailwindcss/typography')]
+};
+
