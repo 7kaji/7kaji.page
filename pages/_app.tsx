f4ccde38e3fb5493bb8719f1c0dea7f4539583a8
7kaji <hirotonbuu@gmail.com>
2020-10-12 20:24:33 +0900

    waving hand icon 👋

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app';
 import 'tailwindcss/tailwind.css';
+import '../styles/index.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
