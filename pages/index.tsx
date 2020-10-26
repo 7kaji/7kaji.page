b77d1591f5816e41601edb4554d0ceea47eded68
7kaji <hirotonbuu@gmail.com>
2020-10-26 10:45:57 +0900

    update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,49 @@ const IndexPage = () => {
 
   return (
     <Layout meta={meta} >
-      <h1>こんにちは <span className="wave">👋</span></h1>
+      <h1>ハロー <span className="wave">👋</span></h1>
+      <ul>
+        <li>
+          <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://twitter.com/7kajii"
+          className="no-underline font-semibold text-gray-700"
+          >
+            twitter
+          </a>
+        </li>
+        <li>
+          <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/7kaji"
+          className="no-underline font-semibold text-gray-700"
+          >
+            github
+          </a>
+        </li>
+        <li>
+          <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://qiita.com/7kaji"
+          className="no-underline font-semibold text-gray-700"
+          >
+            qiita
+          </a>
+        </li>
+        <li>
+          <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://whitech0c0late.hatenablog.com"
+          className="no-underline font-semibold text-gray-700"
+          >
+            hatenablog
+          </a>
+        </li>
+      </ul>
       <p>
         <Link href="/about">
           <a>このサイトについて</a>
